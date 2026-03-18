43ba6229033cdefdb62546ac5968da1c60cc2211
zafarali12 <zafarali12@users.noreply.github.com>
2026-03-19 02:46:40 +0500

    updated files

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -3,9 +3,7 @@ import {
   FaLinkedinIn,
 } from "react-icons/fa6";
 import "./styles/SocialIcons.css";
-import { TbNotes } from "react-icons/tb";
 import { useEffect } from "react";
-import HoverLinks from "./HoverLinks";
 import { zafarData } from "../data/zafarData";
 
 const SocialIcons = () => {
---
+++ src/declarations.d.ts
@@ -0,0 +1,3 @@
+declare module 'gsap-trial/SplitText';
+declare module 'gsap-trial/ScrollSmoother';
+declare module 'gsap-trial/*';
